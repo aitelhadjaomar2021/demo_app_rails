607f3b44095ee69fa3f696aacfe371723c9bab41
Aomar AITELHADJ <aitelhadjaomar14@gmail.com>
2021-09-23 15:30:13 +0100

    Correction branch

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/about' => 'pages#about'
   get '/help' => 'pages#help'
+  get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
